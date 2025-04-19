dc91dd1e60441530ac40207a8a08f46cc787f1e2
Gabriel Salvador <gabosalvador2004@gmail.com>
2025-04-20 02:50:59 +0800

    lab8 debug

--- Lab8/Lab8_Grp2_codeConsumer.cpp
+++ Lab8/Lab8_Grp2_codeConsumer.cpp
@@ -33,19 +33,19 @@ int main(int argc, char *argv[]) {
     int shmSize = stoi(argv[2]); // Get shared memory sizse
 
     // Get Shared Memory
-    int shmidA = shmget(SHM_KEY_A, shmSize, IPC_CREAT | 0666);
+    int shmidA = shmget(SHM_KEY_A, shmSize, 0666);
     if (shmidA == -1) {
         perror("Shared Memory A Error");
         return -1;
     }
-    int shmidB = shmget(SHM_KEY_B, 2, IPC_CREAT | 0666);
+    int shmidB = shmget(SHM_KEY_B, sizeof(shmStatus), 0666);
     if (shmidB == -1) {
         perror("Shared Memory B Error");
         return -1;
     }
 
     // Get Sempahore
-    int semid = semget(SEM_KEY, 1, IPC_CREAT | 0666);
+    int semid = semget(SEM_KEY, 1, 0666);
     if (semid == -1) {
         perror("Semaphore Creation Error");
         return -1;
